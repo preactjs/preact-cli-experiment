aaa5d6872c81648d9b7f91e394fdb9179e3498bb
Nathan Graule <solarliner@gmail.com>
2019-09-07 18:45:10 +0200

    Set normalized parameters back into argv

--- packages/cli/src/plugins/build.ts
+++ packages/cli/src/plugins/build.ts
@@ -14,14 +14,17 @@ export function cli(api: PluginAPI, { packageManager, cwd }: Record<string, stri
 	api.registerCommand("build [src] [dest]")
 		.description("Build the current project into static files")
 		.option("--clean", "Removes destination folder before building", false)
-		.option("-d, --dest", "Destination folder", "dist")
+		.option("--dest", "Destination folder", "build")
 		.option("--no-prerender", "Don't prerender URLs")
 		.option("--production", "Sets the build as production build", false)
 		.option("--brotli", "Enable Brotli compression", false)
 		.action(async (src?: string, dest?: string, argv?: Record<string, any>) => {
-			cwd = argv && argv.cwd ? argv.cwd : cwd;
+			api.debug("argv %O", argv);
+			cwd = argv && argv.cwd !== undefined ? argv.cwd : cwd;
 			src = src || cwd;
-			dest = dest || argv.dest;
+			dest = dest || argv.dest || "build";
+			// Set new values back into argv object
+			Object.assign(argv, { src, dest, cwd });
 			const pm = getPackageManager(packageManager);
 			const modules = path.join(cwd, "node_modules");
 			if (!fs.statSync(modules).isDirectory()) {
