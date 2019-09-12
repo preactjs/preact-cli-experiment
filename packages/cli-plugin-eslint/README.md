e6c0f4ea7c779201627b5b61ef43078d6a4b8364
Nathan Graule <solarliner@gmail.com>
2019-09-12 22:25:19 +0200

    Add eslint plugin

---
+++ packages/cli-plugin-eslint/README.md
@@ -0,0 +1,11 @@
+# `cli-pluguin-eslint`
+
+> TODO: description
+
+## Usage
+
+```
+const cliPluguinEslint = require('cli-pluguin-eslint');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/cli-plugin-eslint/src/index.ts
@@ -0,0 +1,17 @@
+import { PluginAPI } from "@preact/cli";
+
+export const build = (api: PluginAPI) => addEsLint(api, false);
+export const watch = (api: PluginAPI) => addEsLint(api, true);
+
+function addEsLint(api: PluginAPI, watch = false) {
+	api.chainWebpack(chain =>
+		chain.module
+			.rule("eslint")
+			.test(/\.[tj]sx?$/)
+			.exclude.add(/node_modules/)
+			.end()
+			.use("eslint")
+			.loader(require.resolve("eslint-loader"))
+			.options({ fix: true, failOnError: !watch })
+	);
+}
