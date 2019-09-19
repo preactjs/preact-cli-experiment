214cd66be70aed269baef7dc168f6249b4fa1a9e
Nathan Graule <solarliner@gmail.com>
2019-09-19 09:28:47 +0200

    Add build dir to typescript

--- packages/cli-plugin-typescript/src/index.ts
+++ packages/cli-plugin-typescript/src/index.ts
@@ -21,6 +21,7 @@ export async function install(api: PluginAPI, { cwd }: CLIArguments) {
 						target: "es5",
 						module: "esnext",
 						moduleResolution: "node",
+						outDir: "build",
 						allowJs: true,
 						jsx: "react",
 						jsxFactory: "h",
