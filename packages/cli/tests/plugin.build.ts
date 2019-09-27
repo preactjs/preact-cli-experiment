9f5cacdf6e929da7de0952e639af30d08af8f66e
Nathan Graule <solarliner@gmail.com>
2019-09-27 09:08:11 +0200

    Rebuild build snapshots with new directory tree

--- packages/cli/tests/plugin.build.ts
+++ packages/cli/tests/plugin.build.ts
@@ -1,4 +1,4 @@
-import { join } from "path";
+import { join, resolve, relative } from "path";
 import _glob from "glob";
 import { promisify } from "util";
 import test, { ExecutionContext } from "ava";
@@ -6,6 +6,14 @@ import directoryTree from "directory-tree";
 import _rmrf from "rimraf";
 import { createProgram } from "../src/cli";
 import { execAsync } from "../src/utils";
+import { readFile } from "fs";
+import { createHash } from "crypto";
+
+interface DirTreeItem {
+	path: string;
+	size: number;
+	hash: string;
+}
 
 const glob = promisify(_glob);
 const rimraf = promisify(_rmrf);
@@ -26,8 +34,12 @@ async function buildMacro<T>(t: ExecutionContext<T>, input: string, extraArgs: s
 		t.log("Build failed", m.stdout, m.stderr);
 		t.fail();
 	});
-	const tree = directoryTree(join(projectDir, "build"));
-	t.truthy(tree);
+	const tree = await buildDirectoryTree(join(projectDir, "build")).catch(err => {
+		t.log(err);
+		t.fail();
+	});
+	if (!Array.isArray(tree)) return; // For the TypeScript checker
+	t.true(tree.length > 0);
 	t.snapshot(tree);
 }
 
@@ -52,3 +64,31 @@ test(buildMacro, "default");
 test.serial(buildMacro, "prerendering", ["--prerender-urls", "prerender-urls.json"]);
 test.serial(buildMacro, "prerendering", ["--prerender-urls", "prerender-urls.js"]);
 test.serial(buildMacro, "prerendering", ["--prerender-urls", "prerender-urls.promise.js"]);
+
+function buildDirectoryTree(folder: string): Promise<DirTreeItem[]> {
+	folder = resolve(folder);
+	const files = new Array<{ filepath: string; stats: directoryTree.Stats }>();
+	directoryTree(folder, {}, async (item, filepath, stats) => {
+		files.push({ filepath: item.path, stats });
+	});
+	return Promise.all(
+		files.map(async ({ filepath, stats }) => ({
+			path: relative(folder, filepath),
+			size: stats.size,
+			hash: await hashFile(filepath)
+		}))
+	);
+}
+
+async function hashFile(file: string): Promise<string> {
+	return new Promise((resolve, reject) => {
+		readFile(file, (err, data) => {
+			if (err) reject(err);
+			else {
+				const shasum = createHash("sha1");
+				shasum.update(data);
+				resolve(shasum.digest().toString("hex"));
+			}
+		});
+	});
+}
